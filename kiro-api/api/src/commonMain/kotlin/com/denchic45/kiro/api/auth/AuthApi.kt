74fb0736bb2684380282d830aea4337ef102a3bd
Denis <denchic150@gmail.com>
2023-03-07 11:55:30 +0300

    Add api wrappers

--- kiro-api/api/src/commonMain/kotlin/com/denchic45/kiro/api/auth/AuthApi.kt
+++ kiro-api/api/src/commonMain/kotlin/com/denchic45/kiro/api/auth/AuthApi.kt
@@ -11,23 +11,15 @@ import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 
-interface AuthApi {
-    suspend fun signup(signupRequest: SignupRequest): EmptyResponseResult
-
-    suspend fun signInByEmailPassword(signInByEmailPasswordRequest: SignInByEmailPasswordRequest): ResponseResult<TokenResponse>
-
-    suspend fun refreshToken(refreshTokenRequest: RefreshTokenRequest): ResponseResult<TokenResponse>
-}
-
-class AuthApiImpl(private val client: HttpClient) : AuthApi {
-    override suspend fun signup(signupRequest: SignupRequest): EmptyResponseResult {
+class AuthApi(private val client: HttpClient) {
+    suspend fun signup(signupRequest: SignupRequest): EmptyResponseResult {
         return client.post("/auth/signup") {
             contentType(ContentType.Application.Json)
             setBody(signupRequest)
         }.toResult()
     }
 
-    override suspend fun signInByEmailPassword(signInByEmailPasswordRequest: SignInByEmailPasswordRequest): ResponseResult<TokenResponse> {
+    suspend fun signInByEmailPassword(signInByEmailPasswordRequest: SignInByEmailPasswordRequest): ResponseResult<TokenResponse> {
         return client.post("/auth/token") {
             contentType(ContentType.Application.Json)
             setBody(signInByEmailPasswordRequest)
@@ -35,7 +27,7 @@ class AuthApiImpl(private val client: HttpClient) : AuthApi {
         }.toResult()
     }
 
-    override suspend fun refreshToken(refreshTokenRequest: RefreshTokenRequest): ResponseResult<TokenResponse> {
+    suspend fun refreshToken(refreshTokenRequest: RefreshTokenRequest): ResponseResult<TokenResponse> {
         return client.post("/auth/token") {
             contentType(ContentType.Application.Json)
             parameter("grant_type", "refresh_token")
--- kiro-api/api/src/commonMain/kotlin/com/denchic45/kiro/api/user/UserApi.kt
+++ kiro-api/api/src/commonMain/kotlin/com/denchic45/kiro/api/user/UserApi.kt
@@ -10,41 +10,29 @@ import io.ktor.client.request.*
 import io.ktor.http.*
 import java.util.*
 
-interface UserApi {
-    suspend fun create(createUserRequest: CreateUserRequest): ResponseResult<UserResponse>
-
-    suspend fun getMe(): ResponseResult<UserResponse>
-
-    suspend fun getById(userId: UUID): ResponseResult<UserResponse>
-
-    suspend fun search(query: String): ResponseResult<List<UserResponse>>
-
-    suspend fun delete(userId: UUID): EmptyResponseResult
-}
-
-class UserApiImpl(private val client: HttpClient) : UserApi {
-    override suspend fun create(createUserRequest: CreateUserRequest): ResponseResult<UserResponse> {
+class UserApi(private val client: HttpClient) {
+    suspend fun create(createUserRequest: CreateUserRequest): ResponseResult<UserResponse> {
         return client.post("/users") {
             contentType(ContentType.Application.Json)
             setBody(createUserRequest)
         }.toResult()
     }
 
-    override suspend fun getMe(): ResponseResult<UserResponse> {
+    suspend fun getMe(): ResponseResult<UserResponse> {
         return client.get("/users/me").toResult()
     }
 
-    override suspend fun getById(userId: UUID): ResponseResult<UserResponse> {
+    suspend fun getById(userId: UUID): ResponseResult<UserResponse> {
         return client.get("/users/$userId").toResult()
     }
 
-    override suspend fun search(query: String): ResponseResult<List<UserResponse>> {
+    suspend fun search(query: String): ResponseResult<List<UserResponse>> {
         return client.get("/users") {
             parameter("q", query)
         }.toResult()
     }
 
-    override suspend fun delete(userId: UUID): EmptyResponseResult {
+    suspend fun delete(userId: UUID): EmptyResponseResult {
         return client.delete("/users/$userId").toResult()
     }
 }
