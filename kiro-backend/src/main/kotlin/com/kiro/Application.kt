3bafd97aa4c12dd4041b423c1a4bf2e373dffe27
Denis <denchic150@gmail.com>
2023-03-13 19:39:01 +0300

    Update application.conf

--- kiro-backend/src/main/kotlin/com/kiro/Application.kt
+++ kiro-backend/src/main/kotlin/com/kiro/Application.kt
@@ -14,10 +14,8 @@ import io.ktor.server.netty.*
 import kotlinx.coroutines.runBlocking
 import org.koin.ktor.ext.inject
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
+
+fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 //fun Application.module() {
 //    configureSerialization()
